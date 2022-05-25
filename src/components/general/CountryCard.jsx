734b059c196b1d886a204e57ca7e89378fcb7040
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-05-25 10:47:10 +0300

    dispay air quality on home cards

--- src/components/general/CountryCard.jsx
+++ src/components/general/CountryCard.jsx
@@ -5,7 +5,10 @@ const CountryCard = ({ map, country }) => (
   <div className="country-card flex flex--column">
     <i className="ri-arrow-right-circle-line" />
     <img src={map} alt="country map" />
-    <p>{country.country}</p>
+    <div className="card-name flex flex--column">
+      <h2>{country.country}</h2>
+      <p>Air quality: {country.list[0].main.aqi}</p>
+    </div>
   </div>
 );
 
@@ -13,6 +16,11 @@ CountryCard.propTypes = {
   map: PropTypes.string.isRequired,
   country: PropTypes.shape({
     country: PropTypes.string.isRequired,
+    list: PropTypes.shape({
+      main: PropTypes.shape({
+        aqi: PropTypes.string.isRequired,
+      }),
+    }),
   }).isRequired,
 };
 
